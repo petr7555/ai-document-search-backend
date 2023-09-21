c8472b199924b5f8f142470fb52abda65c35cdf8
mariubg <mariubg@stud.ntnu.no>
2023-09-21 16:39:14 +0200

    Added lockout on failed logins

--- ai_document_search_backend/auth.py
+++ ai_document_search_backend/auth.py
@@ -1,15 +1,22 @@
 from typing import Annotated
 
+from datetime import datetime, timedelta
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+
+LOCKOUT_TIME_SECONDS = 60*15
+LOCKOUT_INTERVAL_SECONDS = 60*5
+MAX_LOGIN_ATTEMPTS = 10
 
 
 class User(BaseModel):
     username: str
     email: str
-    full_name: str
+    fullName: str
     disabled: bool
+    disabledTime: datetime
+    failedLogins: list
 
 
 class UserInDB(User):
@@ -19,16 +26,49 @@ class UserInDB(User):
 fake_users_db = {
     "marius": {
         "username": "marius",
-        "full_name": "Marius Berdal Gaalaas",
+        "fullName": "Marius Berdal Gaalaas",
         "email": "marius@gmail.com",
         "password": "123",
         "disabled": False,
+        "disabledTime": 0,
+        "failedLogins": [],
     }
 }
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# Called on failed login
+# Checks how many recent login failures the user has, if over the limit, disable them
+def checkLockout(username: str):
+    user_dict = fake_users_db.get(username)
+    lockoutInterval = timedelta(seconds=LOCKOUT_INTERVAL_SECONDS)
+    now = datetime.utcnow()
+    for loginFail in user_dict["failedLogins"].copy():
+        if loginFail + lockoutInterval < now:
+            user_dict["failedLogins"].remove(loginFail)
+
+    user_dict["failedLogins"].append(datetime.utcnow())
+    if len(user_dict["failedLogins"]) >= MAX_LOGIN_ATTEMPTS:
+        user_dict["disabled"] = True
+        user_dict["disabledTime"] = datetime.utcnow()
+        print("LOCKOUT")
+        return True
+    return False
+
+
+# Called to check if disabled user should be opened again
+def checkLockIn(username: str):
+    user_dict = fake_users_db.get(username)
+    timedeltaExpire = timedelta(seconds=LOCKOUT_TIME_SECONDS)
+    if user_dict["disabled"] and user_dict["disabledTime"] + timedeltaExpire < datetime.utcnow():
+        print("LOCKIN")
+        user_dict["disabled"] = False
+        user_dict["disabled"] = 0
+        return True
+    return False
 
 
 # For authenticating
@@ -39,7 +79,15 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     user = UserInDB(**user_dict)
     password = form_data.password
+    if user.disabled and not checkLockIn(user.username):
+        raise HTTPException(
+            status_code=400, detail="Failed login attempts exceed limit. Account locked."
+        )
     if not password == user.password:
+        if checkLockout(user.username):
+            raise HTTPException(
+                status_code=400, detail="Failed login attempts exceed limit. Account locked."
+            )
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-
+    user_dict["failedLogins"].clear()
     return {"access_token": user.username, "token_type": "bearer"}
--- tests/test_token_endpoint.py
+++ tests/test_token_endpoint.py
@@ -1,23 +1,40 @@
 from fastapi.testclient import TestClient
 
 from ai_document_search_backend.application import app
+from ai_document_search_backend.auth import MAX_LOGIN_ATTEMPTS
 
 client = TestClient(app)
 
 
-def test_valid_authentication():
-    response = client.post("/auth/token", data={"username": "marius", "password": 123})
-    assert response.status_code == 200
-    assert response.json() == {"access_token": "marius", "token_type": "bearer"}
-
-
 def test_invalid_password():
-    response = client.post("/auth/token", data={"username": "marius", "password": 1231})
+    response = client.post("/auth/token", data={"username": "marius", "password": "1231"})
     assert response.status_code == 400
     assert response.json() == {"detail": "Incorrect username or password"}
 
 
 def test_invalid_username():
-    response = client.post("/auth/token", data={"username": "maris", "password": 123})
+    response = client.post("/auth/token", data={"username": "maris", "password": "123"})
     assert response.status_code == 400
     assert response.json() == {"detail": "Incorrect username or password"}
+
+
+def test_valid_authentication():
+    response = client.post("/auth/token", data={"username": "marius", "password": "123"})
+    assert response.status_code == 200
+    assert response.json() == {"access_token": "marius", "token_type": "bearer"}
+
+
+def test_lockout():
+    response = client.post("/auth/token", data={"username": "marius", "password": "123"})
+    assert response.status_code == 200
+    for i in range(MAX_LOGIN_ATTEMPTS - 1):
+        print(i)
+        response = client.post("/auth/token", data={"username": "marius", "password": "123q"})
+        assert response.status_code == 400
+        assert response.json() == {"detail": "Incorrect username or password"}
+    response = client.post("/auth/token", data={"username": "marius", "password": "123q"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Failed login attempts exceed limit. Account locked."}
+    response = client.post("/auth/token", data={"username": "marius", "password": "123"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Failed login attempts exceed limit. Account locked."}
